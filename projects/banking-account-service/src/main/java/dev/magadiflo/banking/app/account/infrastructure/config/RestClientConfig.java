86765bc25503bc9c45c7ee3e64934c05f37a0cfa
Martín Díaz <magadiflo@gmail.com>
2026-03-05 23:04:25 -0500

    Configuración del RestClient — `JdkClientHttpRequestFactory`

--- projects/banking-account-service/src/main/java/dev/magadiflo/banking/app/account/infrastructure/config/RestClientConfig.java
+++ projects/banking-account-service/src/main/java/dev/magadiflo/banking/app/account/infrastructure/config/RestClientConfig.java
@@ -3,9 +3,10 @@ package dev.magadiflo.banking.app.account.infrastructure.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
+import java.net.http.HttpClient;
 import java.time.Duration;
 
 @Configuration
@@ -22,13 +23,16 @@ public class RestClientConfig {
 
     @Bean
     public RestClient exchangeRateRestClient() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofSeconds(this.timeoutSeconds));
-        factory.setReadTimeout(Duration.ofSeconds(this.timeoutSeconds));
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(this.timeoutSeconds)) // timeout de conexión
+                .build();
+
+        JdkClientHttpRequestFactory factory = new JdkClientHttpRequestFactory(httpClient);
+        factory.setReadTimeout(Duration.ofSeconds(this.timeoutSeconds));        // timeout de lectura
 
         return RestClient.builder()
-                .baseUrl(this.baseUrl + "/" + this.apiKey)
-                .requestFactory(factory)
+                .baseUrl(String.format("%s/%s", this.baseUrl, apiKey))
+                .requestFactory(factory) //Cambio importante a partir de Spring Boot 3.4.x porque es ahí donde se consolida y se vuelve una recomendación oficial y más necesaria
                 .defaultHeader("Accept", "application/json")
                 .build();
     }
